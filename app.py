cdc745351189b1615dcbc46ef3588fe969f38cba
Barlian Proc YK <barlianmbg@gmail.com>
2026-09-01 22:38:56 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Sistem Procurement Koperasi YK", layout="wide")
+
+st.title("🛒 Sistem Evaluasi Supplier & Procurement")
+st.subheader("Koperasi YK")
+st.write("Selamat datang di aplikasi Sistem Procurement Koperasi YK. Aplikasi siap dikembangkan!")
